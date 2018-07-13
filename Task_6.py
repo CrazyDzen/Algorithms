e741a87c44969cc7a4e231bf441ab9ea566413bd
CrazyDzen <ovevan89@gmail.com>
2018-07-13 22:14:09 +0300

    https://drive.google.com/open?id=1iNm7vCrQHaDN0mx648wcILKG9E3lB3ZQ

---
+++ Task_6.py
@@ -0,0 +1,8 @@
+﻿# Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+number = int(input('Введите цифру от 1 до 26: '))
+letters = list(map(chr, range(ord('a'), ord('z')+1)))
+a = letters[number - 1]
+print(f'Это буква {a}')
+
+
