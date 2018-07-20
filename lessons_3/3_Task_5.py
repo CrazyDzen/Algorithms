9a6702b39fc1f2f40837c21c7f2931b6edad818a
CrazyDzen <ovevan89@gmail.com>
2018-07-20 12:18:36 +0300

    Task_5

---
+++ lessons_3/3_Task_5.py
@@ -0,0 +1,12 @@
+﻿# В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и
+# позицию в массиве.
+import random
+
+list_ran = [random.randint(-50, 50) for _ in range(20)]
+print(list_ran)
+num = min(list_ran)
+if num < 0:
+    print(f'Наибольшее отрицательное число {num}'
+          f'\nпозиция - {list_ran.index(num)}')
+else:
+    print('В массиве нет отрицательных чисел')
