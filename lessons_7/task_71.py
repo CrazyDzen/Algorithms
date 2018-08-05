d762a34239ab8b655960d7c3174a721353ff412f
CrazyDzen <ovevan89@gmail.com>
2018-08-05 15:34:08 +0300

    lessons_7

---
+++ lessons_7/task_71.py
@@ -0,0 +1,23 @@
+﻿from random import randint
+
+'''
+Отсортировать по убыванию методом «пузырька» одномерный целочисленный массив, заданный случайными 
+числами на промежутке [-100; 100). Вывести на экран исходный и отсортированный массивы.
+'''
+
+
+def bubble_sort(li):
+    for i in range(1, len(li)):
+        n = 0
+        for j in range(0, len(li) - i):
+            if li[j] < li[j+1]:
+                li[j], li[j+1] = li[j+1], li[j]
+                n += 1
+        if n == 0:
+            break
+    return li
+
+
+my_list = [randint(-100, 99) for _ in range(10)]
+print(my_list)
+print(bubble_sort(my_list))
---
+++ lessons_7/task_73.py
@@ -0,0 +1,23 @@
+﻿# Массив размером 2m + 1, где m – натуральное число, заполнен случайным образом. Найти в массиве медиану.
+# Медианой называется элемент ряда, делящий его на две равные части: в одной находятся элементы,
+# которые не меньше медианы, в другой – не больше ее.
+from random import randint
+
+
+def median(list):
+    for el in list:
+        more = 0
+        less = 0
+        for els in list:
+            if el > els:
+                less += 1
+            elif el < els:
+                more += 1
+        if more == less:
+            return el
+    return 'fuck'
+
+
+my_list = [randint(-100, 99) for _ in range(11)]
+print(my_list)
+print(median(my_list))
